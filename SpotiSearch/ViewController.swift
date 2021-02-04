ae5db465a28ba5447b7ff00f7e65177399a5b3de
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2021-02-04 12:04:21 +0100

    Setup commit

---
+++ SpotiSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotiSearch
+//
+//  Created by Reinaldo Villanueva Javierre on 4/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
