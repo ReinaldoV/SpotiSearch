f18895d463e5d205c6882ff888d8eeb00792489f
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2021-02-06 14:04:06 +0100

    Added the Keychain manager

---
+++ SpotiSearch/Utils/SecureData/KeychainManager.swift
@@ -0,0 +1,48 @@
+//
+//  KeychainManager.swift
+//  SpotiSearch
+//
+//  Created by Reinaldo Villanueva Javierre on 6/2/21.
+//
+
+import Foundation
+
+enum KeychainError: Error {
+    case noPassword
+    case unexpectedPasswordData
+    case unhandledError(status: OSStatus)
+}
+
+class KeychainManager {
+    let server = "reinaldovillajavi.SpotiSearch"
+
+    func storeItem(code: String) throws {
+        let password = code.data(using: String.Encoding.utf8)!
+        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+                                    kSecAttrServer as String: server,
+                                    kSecValueData as String: password]
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+    }
+
+    func searchItem() throws -> String {
+        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+                                    kSecAttrServer as String: server,
+                                    kSecMatchLimit as String: kSecMatchLimitOne,
+                                    kSecReturnAttributes as String: true,
+                                    kSecReturnData as String: true]
+
+        var item: CFTypeRef?
+        let status = SecItemCopyMatching(query as CFDictionary, &item)
+        guard status != errSecItemNotFound else { throw KeychainError.noPassword }
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+
+        guard let existingItem = item as? [String : Any],
+            let passwordData = existingItem[kSecValueData as String] as? Data,
+            let password = String(data: passwordData, encoding: String.Encoding.utf8)
+        else {
+            throw KeychainError.unexpectedPasswordData
+        }
+        return password
+    }
+}
