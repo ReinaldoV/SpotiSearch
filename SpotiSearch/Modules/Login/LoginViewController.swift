cfd28d435b26400597f069667acb03763700ab67
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2021-02-07 18:06:23 +0100

    Created a convinience init for the login view controller

--- SpotiSearch/Modules/Login/LoginViewController.swift
+++ SpotiSearch/Modules/Login/LoginViewController.swift
@@ -12,19 +12,24 @@ class LoginViewController: UIViewController {
     var webView = WKWebView()
     var myURL: URL?
 
+    convenience init(loginUrl: URL?) {
+        self.init()
+        self.myURL = loginUrl
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         if let url = self.myURL {
-          let myRequest = URLRequest(url: url)
-          webView.load(myRequest)
+            let myRequest = URLRequest(url: url)
+            webView.load(myRequest)
         }
     }
 
     override func loadView() {
-       let webConfiguration = WKWebViewConfiguration()
-       webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
         webView.navigationDelegate = self
-       view = webView
+        view = webView
     }
 }
 
@@ -34,7 +39,7 @@ extension LoginViewController: WKNavigationDelegate {
                         decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
 
         if navigationAction.request.url?.host == "login-callback",
-           let parameters = navigationAction.request.url?.queryParameters {
+            let parameters = navigationAction.request.url?.queryParameters {
             guard let code = parameters["code"] else {
                 decisionHandler(.cancel)
                 return
