1ea783a06e84fa98f4517f070d3bc4ed63b37fdd
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2021-02-08 15:58:09 +0100

    Added the view and the cell of the collection view

---
+++ SpotiSearch/Modules/Search/Cells/SearchTypeCell.swift
@@ -0,0 +1,33 @@
+//
+//  SearchTypeCell.swift
+//  SpotiSearch
+//
+//  Created by Reinaldo Villanueva Javierre on 8/2/21.
+//
+
+import UIKit
+
+class SearchTypeCell: UICollectionViewCell {
+
+    enum CellType: String {
+        case Top
+        case Tracks
+        case Artist
+        case Album
+    }
+
+    @IBOutlet weak var whiteBackgroundView: UIView!
+    @IBOutlet weak var typeLabel: UILabel!
+
+    func configureCell(type: CellType) {
+        self.typeLabel.text = type.rawValue
+    }
+
+    override func layoutSubviews() {
+        roundImageView()
+    }
+
+    private func roundImageView() {
+        whiteBackgroundView.layer.cornerRadius = whiteBackgroundView.frame.height / 2
+    }
+}
---
+++ SpotiSearch/Modules/Search/SearchViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewController.swift
+//  SpotiSearch
+//
+//  Created by Reinaldo Villanueva Javierre on 8/2/21.
+//
+
+import Foundation
