4f76d92dcb1c55c2d0ca57fee7136a9553fcddc6
Reinaldo Villanueva <reinaldovillajavi@gmail.com>
2021-02-09 20:39:12 +0100

    Added AuthorizationInteractorTests

---
+++ SpotiSearchTests/Modules/Authorization/AuthorizationInteractorTests.swift
@@ -0,0 +1,83 @@
+//
+//  AuthorizationInteractorTests.swift
+//  SpotiSearchTests
+//
+//  Created by Reinaldo Villanueva Javierre on 9/2/21.
+//
+
+import XCTest
+@testable import SpotiSearch
+
+class AuthorizationInteractorTests: XCTestCase {
+
+    var sut: AuthorizationInteractor!
+    var presenterMock: AuthorizationPresenterProtocolMock!
+    var keychainManagerProtocolMock: KeychainManagerProtocolMock!
+    var tokenManagerProtocolMock: TokenManagerProtocolMock!
+
+
+    override func setUp() {
+        super.setUp()
+        createSut()
+    }
+
+    override func tearDown() {
+        releaseSut()
+        super.tearDown()
+    }
+
+    func createSut() {
+        presenterMock = AuthorizationPresenterProtocolMock()
+        keychainManagerProtocolMock = KeychainManagerProtocolMock()
+        tokenManagerProtocolMock = TokenManagerProtocolMock()
+        sut = AuthorizationInteractor(keyChainManager: keychainManagerProtocolMock,
+                                      tokenManager: tokenManagerProtocolMock)
+        sut.presenter = presenterMock
+    }
+
+    func releaseSut() {
+        sut = nil
+    }
+
+    // MARK: - Basic test.
+    func testSutIsntNil() {
+        XCTAssertNotNil(sut, "Sut must not be nil.")
+    }
+
+    func testrequestAuthorizationURL() {
+        sut.requestToken(withAuthCode: "")
+        XCTAssertTrue(tokenManagerProtocolMock.requestTokenWasCalled)
+        XCTAssertTrue(keychainManagerProtocolMock.storeItemWasCalled)
+    }
+}
+
+class KeychainManagerProtocolMock: KeychainManagerProtocol {
+
+    var storeItemWasCalled = false
+
+    func storeItem(code: String) throws {
+        storeItemWasCalled = true
+    }
+
+    func searchItem() throws -> String {
+        return ""
+    }
+
+    func deleteItem() throws {
+
+    }
+}
+
+class TokenManagerProtocolMock: TokenManagerProtocol {
+
+    var requestTokenWasCalled = false
+
+    func requestToken(withAuthorizationCode code: String, onSuccess: @escaping (TokenDTO) -> Void, onError: ((Error?) -> Void)?) {
+        requestTokenWasCalled = true
+        onSuccess(TokenDTO(accessToken: "", expiresIn: 0.0, refreshToken: ""))
+    }
+
+    func refreshToken(token: String, onSuccess: @escaping (TokenDTO) -> Void, onError: ((Error?) -> Void)?) {
+
+    }
+}
